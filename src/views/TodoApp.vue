41c2617b57e0826704b379efdcb7c8cb5067902f
AUrdiales <alejandro.urdiales.93@gmail.com>
2023-11-08 08:46:53 +0100

    use @input and binding instead of v-model

--- src/views/TodoApp.vue
+++ src/views/TodoApp.vue
@@ -3,14 +3,14 @@
     <form class="form-container" @submit.prevent="addTodo">
       <div class="input-container">
         <label>Add a to-do</label>
-        <input class="input" v-model="todoTitle" />
+        <input class="input" @input="onChangeTitle" :value="todoTitle" />
       </div>
       <button class="button--submit">Add</button>
     </form>
     <span v-show="numberOfTodos"> {{ completedPercentage }}% of to-dos completed</span>
     <div class="todo-container" v-for="todo in todos" :key="todo.id">
       <label class="todo-item">
-        <input type="checkbox" @change="completeTodo(todo.id)" :checked="todo.completed" /><span
+        <input type="checkbox" @input="completeTodo(todo.id)" :checked="todo.completed" /><span
           :class="['todo-title', { completed: todo.completed }]"
           >{{ todo.title }}</span
         >
@@ -37,6 +37,8 @@ const addTodo = () => {
     todoTitle.value = ''
   }
 }
+
+const onChangeTitle = (event) => todoTitle.value = event.target.value
 
 const completeTodo = (id) => {
   const todo = todos.value.find((oldTodo) => id === oldTodo.id)
